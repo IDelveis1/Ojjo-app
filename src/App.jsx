00db5e15176a71a285622021143950ec6c8b451f
IDelveis1 <ideltube@gmail.com>
2021-08-08 12:12:26 +0300

    fixed popup

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,15 @@ import "./scss/app.scss";
 import CatalogPage from "./pages/CatalogPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import BurgerPopup from "./components/BurgerPopup";
-import { useSelector } from "react-redux";
 
 function App() {
 
-  const headerPopup = useSelector(({ipods}) => ipods.headerPopup);
 
   return (
     <div className="wrapper">
       <Header />
-      { headerPopup 
-            ? <BurgerPopup /> 
-            : <div>
-              <CatalogPage />
-              <Footer />
-              </div> 
-      }
-      
+      <CatalogPage />
+      <Footer />
     </div>
   );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import BurgerPopup from './BurgerPopup';
 import Categories from './InsideContent/Categories';
 import ContentWithForm from './InsideContent/ContentWithForm';
 import ContentWithRead from './InsideContent/ContentWithRead';
@@ -7,10 +9,11 @@ import Products from './InsideContent/Products';
 
 const Content = ({categoryItems }) => {
 
-    
+    const headerPopup = useSelector(({ipods}) => ipods.headerPopup);
 
     return(
         <main className='page'>
+            {headerPopup && <BurgerPopup />}
             <Categories categoryItems={categoryItems}/>
             <Products />
             <ContentWithForm />
